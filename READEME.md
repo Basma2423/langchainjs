36b1deb403a5112a94e4c796d518e65226277794
Basma2423 <mayarbasma2423@gmail.com>
2025-06-04 08:43:50 +0300

    Add the document, create embeddings and upload to Supabase vector store

---
+++ READEME.md
@@ -0,0 +1,1 @@
+# Learning LangChain js with Scrimba
---
+++ index.js
@@ -0,0 +1,44 @@
+import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter'
+import { readFile } from 'fs/promises'
+import dotenv from 'dotenv'
+import { createClient } from '@supabase/supabase-js'
+import { SupabaseVectorStore } from '@langchain/community/vectorstores/supabase'
+import { HuggingFaceInferenceEmbeddings } from '@langchain/community/embeddings/hf'
+
+dotenv.config()
+
+try {
+  const text = await readFile('scrimba-info.txt', 'utf-8')  
+  const splitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 500
+  })
+  
+  const output = await splitter.createDocuments([text])
+
+  const sbURL = process.env.SUPABASE_URL
+  const sbAPIKey = process.env.SUPABASE_API_KEY
+  const hfAPIKey = process.env.HUGGING_FACE_API_KEY
+
+  const client = createClient(sbURL, sbAPIKey)
+  
+  // pick an embedding model from here: https://huggingface.co/models?pipeline_tag=feature-extraction&sort=trending
+  
+  await SupabaseVectorStore.fromDocuments(
+    output, 
+    new  HuggingFaceInferenceEmbeddings({
+      apiKey: hfAPIKey,
+      model: "intfloat/multilingual-e5-large" 
+    }), 
+    {
+      client,
+      tableName: 'documents',
+    } 
+
+  )
+  
+    console.log('Successfully processed and uploaded the documents to Superbase vector store')
+    console.log(`${output.length} document chunks were created and stored`)
+
+} catch (err) {
+  console.log(err)
+}
